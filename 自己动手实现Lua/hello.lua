03de9ab1f449be7e3869aff75ea8fafe328472de
hlx <lingxu.he@santachi.com.cn>
2022-10-22 11:24:24 +0800

    测试代码

--- 自己动手实现Lua/hello.lua
+++ 自己动手实现Lua/hello.lua
@@ -1,3 +1,14 @@
 function foo()
     print(123)
 end
+
+function fib(n)
+    if n < 2 then
+        return n
+    end
+    return fib(n-2)+fib(n-1)
+end
+
+local start = os.clock()
+fib(35)
+print(os.clock() - start)
